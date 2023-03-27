90c2fe41b1a90ceb8d5fd9b94d737f8575bb4d58
zewdu erkyhun <zewdu444@gmail.com>
2023-03-27 18:55:53 +0300

    Add data, queries and schema sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals
+(
+    id integer primary key NOT NULL,
+    name text ,
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal
 );
