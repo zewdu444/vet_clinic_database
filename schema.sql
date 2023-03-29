3d52455509a73584509d6218ed2f7c5cffc64c1b
zewdu erkyhun <zewdu444@gmail.com>
2023-03-29 21:47:45 +0300

    Add schema, data and queries sql

--- schema.sql
+++ schema.sql
@@ -31,3 +31,25 @@ alter table animals alter column id set default nextval('animal_id_seq');
 ALTER TABLE animals drop column species;
 ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
+
+-- project four--
+CREATE TABLE vets
+(
+    id SERIAL  primary key not null,
+    name text ,
+    age  integer,
+	date_of_graduation date
+);
+
+CREATE TABLE specializations(
+   id SERIAL PRIMARY KEY, 
+  species_id int  references species(id) ,
+  vets_id int  references vets(id)
+);
+
+CREATE TABLE visits (
+     id SERIAL PRIMARY KEY,
+     animals_id INTEGER REFERENCES animals(id),
+     vets_id INTEGER REFERENCES vets(id),
+    date_of_visit date
+);
