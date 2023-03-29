bf572c872eb6f20436a93d632c8a591b268e9966
zewdu erkyhun <zewdu444@gmail.com>
2023-03-29 12:51:22 +0300

    Add schema, data and queries sql

--- schema.sql
+++ schema.sql
@@ -12,3 +12,22 @@ CREATE TABLE animals
 
 -- project two--
 Alter table animals add column species text;
+
+---project three---
+CREATE TABLE owners
+(
+    id SERIAL  primary key,
+    full_name text ,
+    age  integer
+);
+CREATE TABLE species
+(
+    id SERIAL primary key,
+    name text
+);
+
+create sequence animal_id_seq;
+alter table animals alter column id set default nextval('animal_id_seq');
+ALTER TABLE animals drop column species;
+ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
